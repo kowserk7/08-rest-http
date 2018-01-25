e20217a742e261fc4e4800570c7cfd14061e28ea
Koko Kassa <kowserk7@gmail.com>
2018-01-24 21:48:39 -0800

    added server file

---
+++ lab-koko/lib/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const http = require('http');
+const Router = require('./router');
+
+const router = new Router ();
+require('../route/route-note');
+
+const app = http.createServer(router.route());
+
+const server = module.exports = {};
+server.start = (port, cb) => app.listen(port, cb);
+server.stop = (cb) => app.close(cb);
+
